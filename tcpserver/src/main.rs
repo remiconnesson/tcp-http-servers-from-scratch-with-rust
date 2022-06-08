ece7c260f91fe55aa75a032b12a377effd7722ea
remiconnesson <remi.connesson@gmail.com>
2022-06-08 08:31:27 +0000

    Listen to incoming TCP socket connection

---
+++ tcpserver/src/main.rs
@@ -0,0 +1,10 @@
+use std::net::TcpListener;
+
+fn main() {
+    let connection_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    println!("TcpServer running on port 3000.");
+    for stream in connection_listener.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connection established.");
+    }
+}
