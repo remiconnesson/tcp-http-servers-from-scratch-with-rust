d9ec08fa9023427c520484349b0d0f8d6410c903
remiconnesson <remiconnesson2@gmail.com>
2022-06-08 08:49:54 +0000

    Echo back the incoming stream

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,10 +1,14 @@
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
     let connection_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("TcpServer running on port 3000.");
     for stream in connection_listener.incoming() {
-        let _stream = stream.unwrap();
+        let mut stream = stream.unwrap(); // shadow the iteration variable.
         println!("Connection established.");
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        stream.write(&mut buffer).unwrap();
     }
 }
